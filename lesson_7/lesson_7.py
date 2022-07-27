dab45b0f1fd7ccc8acca8057991a1ab8bb3b0022
DimaStantsel <dimastantsel@gmail.com>
2022-07-27 16:59:47 +0300

    fibonacci with for and 0

--- lesson_7/lesson_7.py
+++ lesson_7/lesson_7.py
@@ -1,7 +1,8 @@
 def fibonacci(n: int) -> int:
-    fib_1 = fib_2 = 1
+    fib_1 = 0
+    fib_2 = 1
     print(fib_1, fib_2, end=' ')
-    for el in range(2, n):
+    for _ in range(n - 2):
         fib_1, fib_2 = fib_2, fib_1 + fib_2
         print(fib_2, end=' ')
 
