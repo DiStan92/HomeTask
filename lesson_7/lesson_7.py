2b3ba19133633a3c99ae0fd245d221f3051d27e6
DimaStantsel <dimastantsel@gmail.com>
2022-07-28 14:48:10 +0300

    fibonacci recurtion

--- lesson_7/lesson_7.py
+++ lesson_7/lesson_7.py
@@ -1,9 +1,18 @@
-def fibonacci(n: int) -> int:
-    fib_1 = 0
-    fib_2 = 1
-    print(fib_1, fib_2, end=' ')
-    for _ in range(n - 2):
-        fib_1, fib_2 = fib_2, fib_1 + fib_2
-        print(fib_2, end=' ')
+def fibo_recurtion(n: int) -> int:
+    if n <= 1:
+        return n
+    return fibo_recurtion(n - 1) + fibo_recurtion(n - 2)
 
-fibonacci(10)
+n = int(input('input n: '))
+for i in range(n):
+    print(fibo_recurtion(i), end= ' ')
+
+
+def fibo_list(n, fib_1 = 0, fib_2 = 1, fib_lst = [0]):
+    if n == 1:
+        return fib_lst
+    else:
+        fib_lst.append(fib_1 + fib_2)
+        return fibo_list(n - 1, fib_1 + fib_2, fib_1, fib_lst)
+
+print(fibo_list(10))
