57b7f18c2fd9621f69fd345822335f8ee2e8dc4c
DimaStantsel <dimastantsel@gmail.com>
2022-07-28 16:28:28 +0300

    Revert "fibonacci with for and 0"
    This reverts commit dab45b0f1fd7ccc8acca8057991a1ab8bb3b0022.

--- lesson_7/lesson_7.py
+++ lesson_7/lesson_7.py
@@ -1,8 +1,7 @@
 def fibonacci(n: int) -> int:
-    fib_1 = 0
-    fib_2 = 1
+    fib_1 = fib_2 = 1
     print(fib_1, fib_2, end=' ')
-    for _ in range(n - 2):
+    for el in range(2, n):
         fib_1, fib_2 = fib_2, fib_1 + fib_2
         print(fib_2, end=' ')
 
