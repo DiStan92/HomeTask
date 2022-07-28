37fbb8bb81e1bd788ce6ac1bd83016a55edda9fc
DiStan92 <79769027+DiStan92@users.noreply.github.com>
2022-07-28 18:33:14 +0300

    Delete lesson_7.py

--- lesson_7/lesson_7.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n: int) -> int:
-    fib_1 = fib_2 = 1
-    print(fib_1, fib_2, end=' ')
-    for el in range(2, n):
-        fib_1, fib_2 = fib_2, fib_1 + fib_2
-        print(fib_2, end=' ')
-
-fibonacci(10)
