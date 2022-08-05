08ad003a99fdb7fb457c71a6a2b8b6380b67b437
DiStan92 <79769027+DiStan92@users.noreply.github.com>
2022-08-06 00:24:04 +0300

    class triangle with def create (#8)
    * class triangle with def create
    
    * new def name and create side
    
    * create data and validation
    
    * def create side and sorted

---
+++ lesson_9/Triangle.py
@@ -0,0 +1,35 @@
+from random import random
+
+class Triangle:
+    def __init__(self, side_lst):
+        if len(side_lst) == 3:
+            self.side_lst = side_lst
+        else:
+            self.side_lst = None
+
+    def build_triangle(self):
+        if self.side_lst is not None:
+            if sorted(self.side_lst)[0] + sorted(self.side_lst)[1] > sorted(self.side_lst)[2]:
+                return print('triangle is build')
+            else:
+                return print('triangle is not build')
+        else:
+            return print('input correct side')
+
+def create_side_list(n = 3):
+    side_lst = [int(random()*100) for _ in range(n)]
+    print(side_lst)
+    return side_lst
+
+def create_side(n = 3, side_lst = []):
+    for i in range(1, n + 1):
+        i = input(f'input side_{i}: ')
+        if i.isdigit():
+            side_lst.append(int(i))
+    return side_lst
+
+triangle_1 = Triangle(create_side_list())
+triangle_1.build_triangle()
+
+triangle_2 = Triangle(create_side())
+triangle_2.build_triangle()
