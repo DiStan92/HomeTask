9e01fec806bc55c32086a73413d358633f67bc69
DimaStantsel <dimastantsel@gmail.com>
2022-08-06 00:22:48 +0300

    def create side and sorted

--- lesson_9/Triangle.py
+++ lesson_9/Triangle.py
@@ -6,27 +6,26 @@ class Triangle:
             self.side_lst = side_lst
         else:
             self.side_lst = None
+
     def build_triangle(self):
         if self.side_lst is not None:
-            print(self.side_lst)
-            self.side_lst.sort()
-            if self.side_lst[0] + self.side_lst[1] > self.side_lst[2]:
+            if sorted(self.side_lst)[0] + sorted(self.side_lst)[1] > sorted(self.side_lst)[2]:
                 return print('triangle is build')
             else:
                 return print('triangle is not build')
         else:
-            return print('incorrect data')
+            return print('input correct side')
+
 def create_side_list(n = 3):
     side_lst = [int(random()*100) for _ in range(n)]
     print(side_lst)
     return side_lst
+
 def create_side(n = 3, side_lst = []):
     for i in range(1, n + 1):
         i = input(f'input side_{i}: ')
         if i.isdigit():
             side_lst.append(int(i))
-        else:
-            print('error')
     return side_lst
 
 triangle_1 = Triangle(create_side_list())
