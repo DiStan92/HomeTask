c2faaf0c353db70e0068d88bf92b57ea0709a14b
DimaStantsel <dimastantsel@gmail.com>
2022-08-09 00:40:20 +0300

    more magic methods

--- lesson_10/lesson_10.py
+++ lesson_10/lesson_10.py
@@ -14,6 +14,12 @@ class Father(Family):
     def __str__(self):
         return f'Father: {self.syrname} {self.name} is {self.age} years'
 
+    def __sub__(self, other):
+        return f'{self.name} older {mother.name} on {self.age - other.age} years'
+
+    def __repr__(self):
+        return f'{self.name}, {self.age}'
+
 class Mother(Family):
     def __init__(self, syrname, name, age):
         super().__init__(syrname)
@@ -22,6 +28,9 @@ class Mother(Family):
 
     def __str__(self):
         return f'Mother: {self.syrname} {self.name} is {self.age} years'
+
+    def __repr__(self):
+        return f'{self.name}, {self.age}'
 
 class Son(Father, Mother):
     def __init__(self, syrname, name, age, father, mother):
@@ -34,6 +43,9 @@ class Son(Father, Mother):
     def __str__(self):
         return f'{self.syrname} {self.name}, my father is {self.father}, my mother is {self.mother}'
 
+    def __repr__(self):
+        return f'{self.name}, {self.age}'
+
 class Dougther(Father, Mother):
     def __init__(self, syrname, name, age, father, mother):
         self.syrname = syrname
@@ -45,12 +57,24 @@ class Dougther(Father, Mother):
     def __str__(self):
         return f'{self.syrname} {self.name}, my father is {self.father}, my mother is {self.mother}'
 
+    def __sub__(self, other):
+        return f'{self.name} older {son.name} on {self.age - other.age} years'
+
+    def __repr__(self):
+        return f'{self.name}, {self.age}'
+
 family = Family('Stantsel')
 father = Father(family.syrname, 'Dima', 30)
-print(father)
 mother = Mother(family.syrname, 'Diana', 28)
-print(mother)
 son = Son(family.syrname, 'Kirill', 1, father.name, mother.name)
-print(son)
 doughter = Dougther(family.syrname, 'Alene', 5, father.name, mother.name)
+print(father)
+print(mother)
+print(son)
 print(doughter.father)
+print(father - mother)
+print(doughter - son)
+print(repr(father))
+print(repr(mother))
+print(repr(son))
+print(repr(doughter))
