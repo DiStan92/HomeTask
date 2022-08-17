c6515721953259ad5a0177b294ed7d72b72ea10c
DimaStantsel <dimastantsel@gmail.com>
2022-08-17 23:14:31 +0300

    app with import

---
+++ lesson_13/services/annotation.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class ConsumerAnnotation:
+    name: str
+    account: float
+
+
+@dataclass
+class HouseAnnotation:
+    price: float
+    square: float
---
+++ lesson_13/services/recomendation.py
@@ -0,0 +1,12 @@
+from annotation import HouseAnnotation, ConsumerAnnotation
+from generation import house_gener, consumer_gener
+
+
+def get_recommendations(house_lst: list[HouseAnnotation], consumer: ConsumerAnnotation) -> dict[
+    ConsumerAnnotation, HouseAnnotation]:
+    rec_house = [house for house in house_lst if house.price < consumer.account]
+    rec_house.sort(key=lambda x: x.square)
+    return {"Consumer": consumer, "Recommendations": rec_house[::-1]}
+
+
+print((get_recommendations(house_gener(10), consumer_gener())))
