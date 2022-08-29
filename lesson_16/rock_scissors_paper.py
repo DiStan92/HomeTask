816ec77e1f4ed2f5bf00f66cc9d4a1877bc94bfa
DimaStantsel <dimastantsel@gmail.com>
2022-08-29 23:50:44 +0300

    debug

--- lesson_16/rock_scissors_paper.py
+++ lesson_16/rock_scissors_paper.py
@@ -17,7 +17,7 @@ def game(player_1: str, player_2: str, player_3: str):
         return print(f"{player_3} and {player_2} is winners, {player_1} is lose")
     elif player_1 and player_3 not in WIN_DICTONARY[player_2]:
         return print(f"{player_1} and {player_3} is winners, {player_2} is lose")
-    elif player_1 and player_2 in WIN_DICTONARY[player_3]:
+    elif player_1 and player_2 not in WIN_DICTONARY[player_3]:
         return print(f"{player_1} and {player_2} is winners, {player_3} is lose")
 
 
