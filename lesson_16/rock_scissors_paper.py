2ac4d899105f0db11d50a18667ecac9b939477d1
DimaStantsel <dimastantsel@gmail.com>
2022-09-01 00:52:13 +0300

    random player and def

--- lesson_16/rock_scissors_paper.py
+++ lesson_16/rock_scissors_paper.py
@@ -1,29 +1,44 @@
+from random import randint
+
+TOOLS = ['rock', 'scissors', 'paper']
 WIN_DICTONARY = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
 
 
 def game(player_1: str, player_2: str, player_3: str):
-    if player_1 == player_2 == player_3:
-        return print("Tie")
-    elif player_1 != player_2 != player_3:
-        return print("Tie")
-    #two same players
-    elif player_3 and player_2 in WIN_DICTONARY[player_1]:
-        return print(f"{player_1} is winner, other: Tie")
-    elif player_1 and player_3 in WIN_DICTONARY[player_2]:
-        return print(f"{player_2} is winner, other: Tie")
-    elif player_1 and player_2 in WIN_DICTONARY[player_3]:
-        return print(f"{player_3} is winner, other: Tie")
-    elif player_3 and player_2 not in WIN_DICTONARY[player_1]:
-        return print(f"{player_3} and {player_2} is winners, {player_1} is lose")
-    elif player_1 and player_3 not in WIN_DICTONARY[player_2]:
-        return print(f"{player_1} and {player_3} is winners, {player_2} is lose")
-    elif player_1 and player_2 not in WIN_DICTONARY[player_3]:
-        return print(f"{player_1} and {player_2} is winners, {player_3} is lose")
+    print(player_1, player_2, player_3)
+    different_player = list(set([player_1, player_2, player_3]))
+    if len(different_player) == 1 or len(different_player) == 3:
+        print("Tie")
+        return game(*gen_players())
+    elif player_1 == player_2 and player_1 in WIN_DICTONARY[player_3]:
+        return print(f'player_3 {player_3} is winner')
+    elif player_1 == player_3 and player_3 in WIN_DICTONARY[player_2]:
+        return print(f'player_2 {player_2} is winner')
+    elif player_3 == player_2 and player_3 in WIN_DICTONARY[player_1]:
+        return print(f'player_1 {player_1} is winner')
+    else:
+        return game_2(*gen_player_new())
 
 
-game('rock', 'rock', 'rock')
-game('rock', 'scissors', 'paper')
-game('rock', 'scissors', 'scissors')
-game('scissors', 'paper', 'paper')
-game('rock', 'paper', 'paper')
-game('paper', 'scissors', 'scissors')
+def game_2(player_1, player_2):
+    print(player_1, player_2)
+    if player_1 == player_2:
+        print('Tie')
+        return game_2(*gen_player_new())
+    elif player_1 in WIN_DICTONARY[player_2]:
+        return print(f'player_2 {player_2} is winner')
+    else:
+        return print(f'player_1 {player_1} is winner')
+
+
+def gen_players():
+    res = [TOOLS[randint(0, 2)] for _ in range(3)]
+    return res
+
+
+def gen_player_new():
+    res = [TOOLS[randint(0, 2)] for _ in range(2)]
+    return res
+
+
+game(*gen_players())
